b5ed10d2780d66e34dfaa04d4976829e5db8565a
thier32 <thier32@email.com>
2026-03-30 02:26:45 +0200

    Adding testing

---
+++ FunctionApp1.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace FunctionApp1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestAddition()
+        {
+            // Un test simple pour vérifier que le pipeline fonctionne
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
